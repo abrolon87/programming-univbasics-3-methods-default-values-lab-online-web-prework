950596ae4c4de60bd67600939dbd0b1997dcafd7
Amanda R <abrolon87@gmail.com>
2019-11-07 17:21:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,12 +1,12 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
-def meal_choice(w, y, protein = "meat")
+def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
-  Return "A plate of #{protein} with #{w} and #{y}."
+   "A plate of #{protein} with #{veg1} and #{veg2}."
 
 
 
 end
 
 
-meal_choice("broccoli", "macaroni", "tofu")
+meal_choice("w", "y", "tofu")
